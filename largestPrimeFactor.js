09d20d53c8073ea77abde43dcef7856f491722e0
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-05-24 08:15:11 -1000

    used while loop

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,14 @@
 exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
+  //var primeNumber = 0;
+  var primeNumber = 2;
   // do your work here
+  while (primeNumber <=n) {
+    if(n % primeNumber === 0) {
+      n /= primeNumber;
+    } else {
+      primeNumber++;
+    }
+  }
 
   return primeNumber;
 };
